fffe5eb80fbbc75283b73695f7988120ee94f96e
Ben Balter <ben.balter@github.com>
2016-12-30 13:58:37 -0500

    reset the cache before each render

--- lib/jekyll-include-cache.rb
+++ lib/jekyll-include-cache.rb
@@ -3,9 +3,20 @@ require "jekyll"
 module JekyllIncludeCache
   autoload :Tag, "jekyll-include-cache/tag"
 
-  def self.cache
-    @cache ||= {}
+  class << self
+    attr_writer :cache
+
+    def cache
+      @cache ||= {}
+    end
+
+    def reset
+      JekyllIncludeCache.cache = {}
+    end
   end
 end
 
 Liquid::Template.register_tag("include_cached", JekyllIncludeCache::Tag)
+Jekyll::Hooks.register :site, :pre_render do |_site|
+  JekyllIncludeCache.reset
+end
--- spec/jekyll-include-tag_spec.rb
+++ spec/jekyll-include-tag_spec.rb
@@ -9,13 +9,24 @@ RSpec.describe JekyllIncludeCache do
   end
 
   context "with something cached" do
-    before do
-      described_class.instance_variable_set("@cache", { "foo" => "bar" })
-    end
+    before { described_class.cache = { "foo" => "bar" } }
 
     it "returns the cache" do
       expect(described_class.cache).to have_key("foo")
       expect(described_class.cache["foo"]).to eql("bar")
+    end
+  end
+
+  context "clearing the cache on render" do
+    let(:site) { fixture_site("site") }
+
+    before do
+      described_class.instance_variable_set("@cache", { "foo" => "bar" })
+      Jekyll::Hooks.trigger :site, :pre_render, site, site.site_payload
+    end
+
+    it "clears the cache" do
+      expect(described_class.cache).to eql({})
     end
   end
 end
